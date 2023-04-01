abd5ed26bea452cdefec7c477ce40360573adc68
jjj <wown7673@naver.com>
2023-04-01 15:36:04 +0900

    다시 정리함 정상작동함

--- app/server.js
+++ app/server.js
@@ -8,15 +8,19 @@ const app =  express();
 
 
 // 라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 미들웨어를 등록해주는 메서드 ( app.METHOD 와 비슷?)  // 따라서 / 요청을 home경로로 돌려준다?
 app.use('/', home);  
-                   
+    
+// 미들웨어 등록
+// __dirname : 현재 이 파일이 있는 위치 
+// 정적파일들은 아래 경로를 기본 경로로 설정함
+//app.use(express.static(__dirname+'/src/public'));  
 
 
 // 뷰 설정
-app.set('views',__dirname + '/views');    
+app.set('views',__dirname + '/src/views');    
 // ejs 뷰엔진
 app.set('view engine', 'ejs');     
 
